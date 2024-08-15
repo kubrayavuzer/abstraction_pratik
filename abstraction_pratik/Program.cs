eae3c3016f7a90eb8aaddb9142900a9058b1605c
kubra <kubra@DESKTOP-K1F6O3C>
2024-08-15 20:48:58 +0300

    Proje dosyası ekle.

---
+++ abstraction_pratik/Program.cs
@@ -0,0 +1,25 @@
+﻿using abstraction_pratik;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //yazılım geliştirici nesnesi oluşturma
+
+        YazilimGelistirici yazilimgelistirici = new YazilimGelistirici("ahmet", "mehmet", "yazılım");
+
+        yazilimgelistirici.Gorev();
+
+        //proje yöneticisi nesnesi oluşturma
+
+        ProjeYoneticisi projeYoneticisi = new ProjeYoneticisi("hasan", "hüseyin", "proje yöneticisi");
+        projeYoneticisi.Gorev();
+
+        //satış temsilcisi nesnesi oluşturma
+
+        SatisDanismani satisDanismani = new SatisDanismani("ayşe", "hülya", "satış danışmanı");
+        satisDanismani.Gorev();
+
+    }
+
+}
+
---
+++ abstraction_pratik/SirketCalisanlari.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstraction_pratik
+{
+    public abstract class SirketCalisanlari
+    {
+        //çalışanların ortak özellikleri
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Departman { get; set; }
+
+        //kurucu metot
+        public SirketCalisanlari(string ad, string soyad, string departman)
+        {
+            Ad = ad;
+            Soyad = soyad;
+            Departman = departman;
+        }
+
+        public abstract void Gorev();
+
+    }
+
+    public class YazilimGelistirici : SirketCalisanlari
+    {
+        public YazilimGelistirici(string ad, string soyad, string departman)
+            : base(ad, soyad, departman) { }
+
+        public override void Gorev()
+        {
+            Console.WriteLine($" {Ad} {Soyad} yazılım geliştirici olarak çalışıyorsunuz.");
+        }
+
+    }
+
+    public class ProjeYoneticisi : SirketCalisanlari
+    {
+        public ProjeYoneticisi(string ad, string soyad, string departman)
+            : base(ad, soyad, departman) { }
+
+        public override void Gorev()
+        {
+            Console.WriteLine($" {Ad} {Soyad} proje yöneticisi olarak çalışıyorsunuzz.");
+
+        }
+    }
+    public class SatisDanismani : SirketCalisanlari
+    {
+        public SatisDanismani(string ad, string soyad, string departman)
+            : base(ad, soyad, departman) { }
+
+        public override void Gorev()
+        {
+            Console.WriteLine($"{Ad} {Soyad} satış danışmanı olarak çalışıyorsunuz.");
+        }
+    }
+
+
+}
